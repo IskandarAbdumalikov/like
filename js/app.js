44ac9a27cb71252550f6e3f96971f2ffcca936bd
iskandarprogrammer2007 <abdumalikoviskandar600@gmail.com>
2024-04-28 22:10:44 +0500

    added

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ let likes = document.querySelector(".like__counts");
 const LIMIT_COUNT = 4;
 let count = 1;
 let wrapper = document.querySelector(".wrapper");
-let likesCount = JSON.parse(localStorage.getItem("wishlist")).length
+let likesCount = 0
 console.log(likesCount);
 // Funksiyalar
 async function fetchData(api) {
